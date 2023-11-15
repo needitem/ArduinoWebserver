1f5fe68974e2e157f039300db3611ab98cfa637b
needitem <th07290828@gmail.com>
2023-11-15 11:38:47 +0900

    aa

--- python/pythonhub.py
+++ python/pythonhub.py
@@ -1,23 +1,28 @@
 from serial import Serial
 import time
 import os
-os.environ['PYTHONUNBUFFERED'] = '1'
+
+os.environ["PYTHONUNBUFFERED"] = "1"
+
 
 class PythonHub:
-    def __init__(self, comPort='COM3', baudrate=9600, timeout=1):
+    def __init__(self, comPort="/dev/ttyACM0", baudrate=9600, timeout=1):
         try:
             self.ser = Serial(comPort, baudrate, timeout=timeout)
+            self.clearSerial()
+            self.clearVoltTuple()
+
         except:
             print("Failed to open serial port:", comPort)
             print("Please check the port name and try again.")
             exit(1)
-            
+
         print("Serial port:", self.ser.name)
-    
+
     def writeSerial(self, *args):
-        sCmd = ' '.join(args) + '\n'
-        
-        #debug input
+        sCmd = " ".join(args) + "\n"
+
+        # debug input
         print("Sending:", sCmd)
 
         self.ser.write(sCmd.encode())
@@ -31,9 +36,9 @@ class PythonHub:
     def talk(self):
         while True:
             sCmd = input("Enter command or 'exit':")
-            if sCmd == 'exit':
+            if sCmd == "exit":
                 break
-            elif sCmd == 'help':
+            elif sCmd == "help":
                 self.help()
             else:
                 try:
@@ -43,7 +48,7 @@ class PythonHub:
                     print("Failed to send command:", sCmd)
                     print("Please check the command and try again.")
 
-    def clearSerial(self): 
+    def clearSerial(self):
         self.ser.flushInput()
         self.ser.flushOutput()
 
@@ -66,43 +71,56 @@ class PythonHub:
 
     def getVolt(self):
         try:
-            volt =  self.writeSerial('get', 'volt')
+            volt = self.writeSerial("get", "volt")
             volt = float(volt)
-            return volt                  
+            return volt
         except:
             pass
-    
+
     def getLight(self):
-        try:  
-            light = self.writeSerial('get', 'light')
+        try:
+            light = self.writeSerial("get", "light")
             light = float(light)
-            print("Received: ",light)
+            print("Received: ", light)
             return light
         except:
             pass
 
     def setServo(self, angle):
         try:
-            self.writeSerial('set', 'servo', str(angle))
+            self.writeSerial("set", "servo", str(angle))
         except:
             pass
-    
+
     def setLed(self, color):
         try:
-            self.writeSerial('set', 'led', color)
+            self.writeSerial("set", "led", color)
+        except:
+            print("Failed to set LED color:", color)
+
+    def setBuzzer(self, freq, duration):
+        try:
+            self.writeSerial("set", "buzzer", str(freq), str(duration))
         except:
             pass
 
-    def setBuzzer(self, freq, duration):
-        if freq.isdigit() == True:
-            try:
-                self.writeSerial('set', 'buzzer', str(freq), str(duration))
-            except:
-                pass
+    def addVoltTuple(self):
+        volt = self.getVolt()
+        voltTime = time.time()
+        if volt >= 0:
+            self.volts += (volt,)
+            self.voltTimes += (voltTime,)
+            return True
         else:
-            self.writeSerial('set', 'buzzer', freq, str(duration))
-        
+            print("Failed to get voltage reading.")
+            print("Please check the sensor connection.")
+            return False
+
+    def clearVoltTuple(self):
+        self.volts = ()
+        self.voltTimes = ()
 
 
-
-    
+    def printVoltTuple(self):
+        for volt, times in zip(self.volts, self.voltTimes):
+            print(f"volt: {volt:.2f}V, time: {time.ctime(times)}")
